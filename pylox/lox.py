4c446edbd52f31c64040997a765b4b8d104eac77
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2023-07-14 16:48:42 +0200

    section 4.1

---
+++ pylox/lox.py
@@ -0,0 +1,37 @@
+import argparse
+
+
+def run(src: str) -> None:
+    print(src.split(" "))
+
+
+def run_file(script_path: str):
+    with open(script_path, "rt") as f:
+        run(f.read())
+
+
+def run_prompt():
+    while True:
+        try:
+            line = input("> ")
+            run(line)
+        except EOFError:
+            print("\nsee you later :)")
+            break
+
+
+def main(script_path: str) -> None:
+    if len(script_path) > 0:
+        run_file(script_path)
+    else:
+        run_prompt()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="pylox",
+        description="lox language interperter",
+    )
+    parser.add_argument("-s", "--script", required=False, default="")
+    args = parser.parse_args()
+    main(args.script)
