90f1eac158f731f153d31507b54758bb18f93b67
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2023-07-14 17:17:48 +0200

    Added dict with tokens

---
+++ pylox/tokenTypes.py
@@ -0,0 +1,51 @@
+from enum import Enum, auto
+
+
+class Token_type(Enum):
+    # single-character tokens
+    LEFT_PAREN = auto()
+    RIGHT_PAREN = auto()
+    LEFT_BRACE = auto()
+    RIGHT_BRACE = auto()
+    COMMA = auto()
+    DOT = auto()
+    MINUS = auto()
+    PLUS = auto()
+    SEMICOLON = auto()
+    SLASH = auto()
+    STAR = auto()
+
+    # multi-character token
+    BANG = auto()
+    BANG_EQUAL = auto()
+    EQUAL = auto()
+    EQUAL_EQUAL = auto()
+    GREATER = auto()
+    GREATER_EQUAL = auto()
+    LESS = auto()
+    LESS_EQUAL = auto()
+
+    # LITERALS
+    IDENTIFIER = auto()
+    STRING = auto()
+    NUMBER = auto()
+
+    # KEYWORDS
+    AND = auto()
+    CLASS = auto()
+    ELSE = auto()
+    FALSE = auto()
+    FUN = auto()
+    FOR = auto()
+    IF = auto()
+    NIL = auto()
+    OR = auto()
+    PRINT = auto()
+    RETURN = auto()
+    SUPER = auto()
+    THIS = auto()
+    TRUE = auto()
+    VAR = auto()
+    WHILE = auto()
+
+    EOF = auto()
