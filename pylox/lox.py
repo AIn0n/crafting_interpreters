3ffb1ef04e1e94590c28c13e19ca538e3781a6ce
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2023-07-14 17:08:33 +0200

    section 4.2 done, but without hadError flag - leave it to the future

--- pylox/lox.py
+++ pylox/lox.py
@@ -1,6 +1,14 @@
 import argparse
 
 
+def report(line: int, where: str, msg: str) -> None:
+    print(f"[line {line}] Error {where}: {msg}")
+
+
+def error(line: int, msg: str) -> None:
+    report(line, "", msg)
+
+
 def run(src: str) -> None:
     print(src.split(" "))
 
