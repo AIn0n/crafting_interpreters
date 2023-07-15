efcb5ccbc0385c4674ecfa7c77fc8bdc0f057700
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2023-07-15 12:13:42 +0200

    added token class (section 4.4)

---
+++ pylox/token.py
@@ -0,0 +1,12 @@
+from tokenTypes import Token_type
+
+
+class Token:
+    def __init__(self, ttype: Token_type, lexeme: str, literal, line: int):
+        self.ttype = ttype
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+
+    def __str__(self) -> str:
+        return f"{self.ttype} {self.lexeme} {self.literal}"
