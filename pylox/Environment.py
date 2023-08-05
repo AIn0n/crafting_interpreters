9b3f85590bde81fe057867757207d7fa7b802e4d
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2023-08-05 15:57:09 +0200

    challenge 2 finished

--- pylox/Environment.py
+++ pylox/Environment.py
@@ -21,7 +21,10 @@ class Environment:
 
     def get(self, name):
         if name.lexeme in self.values:
-            return self.values[name.lexeme]
+            res = self.values[name.lexeme]
+            if res is None:
+                raise Runtime_lox_error(name, f"variable {name.lexeme} not initalized")
+            return res
 
         if self.enclosing is not None:
             return self.enclosing.get(name)
