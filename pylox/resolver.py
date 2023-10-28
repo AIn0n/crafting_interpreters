ac2aa20bee9e428f4d0f429eda2eb9bfcfe9d803
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2023-10-28 21:39:17 +0200

    Finished section 11.3.1

--- pylox/resolver.py
+++ pylox/resolver.py
@@ -1,8 +1,31 @@
-from Stmt import VisitorStmt
-from Expr import VisitorExpr
+from Stmt import VisitorStmt, Stmt
+from Expr import VisitorExpr, Expr
 from interpreter import Interpreter
+from typing import Mapping
 
 
 class Resolver(VisitorExpr, VisitorStmt):
     def __init__(self, interpreter: Interpreter) -> None:
         self.interpreter = interpreter
+        self.scopes: list[Mapping] = []
+
+    def beginScope(self) -> None:
+        self.scopes.append({})
+
+    def endScope(self) -> None:
+        self.scopes.pop()
+
+    def resolve_expr(self, expr: Expr):
+        expr.accept(self)
+
+    def resolve_stmt(self, stmt: Stmt):
+        stmt.accept(self)
+
+    def resolve_statements(self, statements):
+        for stmt in statements:
+            self.resolve_stmt(stmt)
+
+    def visitBlock(self, stmt) -> None:
+        self.beginScope()
+        self.resolve_statements(stmt.statements)
+        self.endScope()
