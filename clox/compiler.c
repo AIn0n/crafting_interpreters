37749ff79463bc8f73e4f3af175e35ff5345075f
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2024-01-19 17:42:06 +0100

    Started section 16.2 -- added basic token data structures

--- clox/compiler.c
+++ clox/compiler.c
@@ -7,5 +7,17 @@
 void
 compile(const char *src)
 {
+	int line = -1;
 	init_scanner(src);
+	for (;;) {
+		Token token = scan_token();
+		if (token.line != line) {
+			printf("%4d ", token.line);
+			line = token.line;
+		} else {
+			printf("   | ");
+		}
+		printf("%2d '%.*s'\n", token.type, token.len, token.start);
+		if (token.type == TOKEN_EOF) break;
+	}
 }
