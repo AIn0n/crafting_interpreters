8dc4aae99e6c67f6390a5c05d4a8ca9ca27145f1
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2024-01-20 19:17:05 +0100

    finished section 16.2.1 -- added scanner helper functions

--- clox/scanner.c
+++ clox/scanner.c
@@ -19,3 +19,41 @@ init_scanner(const char *src)
 	scanner.curr = src;
 	scanner.line = 1;
 }
+
+static bool 
+is_at_end(void)
+{
+	return *scanner.curr == '\0';
+}
+
+static Token
+make_token(TokenType type)
+{
+	return (Token) {
+		.type = type,
+		.start = scanner.start,
+		.len = (int) (scanner.curr - scanner.start),
+		.line = scanner.line
+	};
+}
+
+static Token
+error_token(const char *msg)
+{
+	return (Token) {
+		.type = TOKEN_ERROR,
+		.start = msg,
+		.len = (int) strlen(msg),
+		.line = scanner.line
+	};
+}
+
+Token
+scan_token(void)
+{
+	scanner.start = scanner.curr;
+
+	if (is_at_end()) return make_token(TOKEN_EOF);
+
+	return error_token("unexpected character");
+}
