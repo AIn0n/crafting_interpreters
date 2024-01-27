9cfe0e01a648ccbd62609dc1d5c79a8795f72a6f
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2024-01-27 12:07:28 +0100

    started section 16.3 -- added few first, one char tokens to the scanner

--- clox/scanner.c
+++ clox/scanner.c
@@ -48,12 +48,62 @@ error_token(const char *msg)
 	};
 }
 
+static char
+advance()
+{
+	scanner.curr++;
+	return scanner.curr[-1];
+}
+
+static bool match(const char expected) {
+	if (is_at_end())
+		return false;
+	if (*scanner.curr != expected)
+		return false;
+	scanner.curr++;
+	return true;
+}
+
 Token
 scan_token(void)
 {
 	scanner.start = scanner.curr;
 
 	if (is_at_end()) return make_token(TOKEN_EOF);
+
+	char c = advance();
+	switch (c) {
+	case '(': 
+		return make_token(TOKEN_LEFT_PAREN);
+	case ')': 
+		return make_token(TOKEN_RIGHT_PAREN);
+	case '{': 
+		return make_token(TOKEN_LEFT_BRACE);
+	case '}': 
+		return make_token(TOKEN_RIGHT_BRACE);
+	case ';': 
+		return make_token(TOKEN_SEMICOLON);
+	case ',': 
+		return make_token(TOKEN_COMMA);
+	case '.':
+		return make_token(TOKEN_DOT);
+	case '-':
+		return make_token(TOKEN_MINUS);
+	case '+':
+		return make_token(TOKEN_PLUS);
+	case '/':
+		return make_token(TOKEN_SLASH);
+	case '*':
+		return make_token(TOKEN_STAR);
+	case '!':
+		return make_token(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+	case '=':
+		return make_token(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+	case '<':
+		return make_token(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+	case '>':
+		return make_token(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+	}
 
 	return error_token("unexpected character");
 }
